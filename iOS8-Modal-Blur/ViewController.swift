e95aa7b1c7f232aac13b0788d1d7e09590ca9ba1
Nicky Tones <nicktones@icloud.com>
2016-04-12 10:25:02 +0100

    Initial Commit

---
+++ iOS8-Modal-Blur/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS8-Modal-Blur
+//
+//  Created by Nicky Tones on 12/04/2016.
+//  Copyright © 2016 TonesTalks-Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
